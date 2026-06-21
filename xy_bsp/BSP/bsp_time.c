4eeff640dab9703e9168e115da8081ab66cdd936
xiaoyi <2965695492@qq.com>
2026-06-21 23:00:59 +0800

    封装HAL时基

---
+++ xy_bsp/BSP/bsp_time.c
@@ -0,0 +1,7 @@
+#include "bsp_time.h"
+
+uint32_t BSP_GetTick(void)
+{
+    return uwTick;
+}
+
---
+++ xy_bsp/BSP/bsp_time.h
@@ -0,0 +1,8 @@
+#ifndef __BSP_TIME_H
+#define __BSP_TIME_H
+
+#include "main.h"
+
+uint32_t BSP_GetTick(void);
+
+#endif
