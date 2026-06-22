27ecbfb5f81177bfb8a480350c9155c91e2c71e6
xiaoyi <2965695492@qq.com>
2026-06-22 14:06:03 +0800

    更新写入电平，修改读取按键返回值，增加电平封装

--- xy_bsp/BSP/bsp_gpio.c
+++ xy_bsp/BSP/bsp_gpio.c
@@ -1,36 +1,28 @@
 #include "bsp_gpio.h"
 
-void BSP_GPIO_Set(BSP_GPIO_t *gpio)
-{
-    HAL_GPIO_WritePin(
-        gpio->Port,
-        gpio->Pin,
-        GPIO_PIN_SET
-    );
-}
 
-void BSP_GPIO_Reset(BSP_GPIO_t *gpio)
+
+void BSP_GPIO_Write(BSP_GPIO_t *gpio, GPIO_Level_t level)
 {
-    HAL_GPIO_WritePin(
-        gpio->Port,
-        gpio->Pin,
-        GPIO_PIN_RESET
-    );
+    HAL_GPIO_WritePin(gpio->Port,gpio->Pin,level ? GPIO_PIN_SET : GPIO_PIN_RESET);
 }
 
 void BSP_GPIO_Toggle(BSP_GPIO_t *gpio)
 {
-    HAL_GPIO_TogglePin(
-        gpio->Port,
-        gpio->Pin
-    );
+    HAL_GPIO_TogglePin(gpio->Port,gpio->Pin);
 }
 
-GPIO_PinState BSP_GPIO_Read(BSP_GPIO_t *gpio)
+
+
+GPIO_Level_t BSP_GPIO_Read(BSP_GPIO_t *gpio)
 {
-    return HAL_GPIO_ReadPin(
-        gpio->Port,
-        gpio->Pin
-    );
+    if (HAL_GPIO_ReadPin(gpio->Port, gpio->Pin) == GPIO_PIN_SET)
+    {
+        return GPIO_HIGH;
+    }
+    else
+    {
+        return GPIO_LOW;
+    }
 }
 
--- xy_bsp/BSP/bsp_gpio.h
+++ xy_bsp/BSP/bsp_gpio.h
@@ -7,13 +7,17 @@ typedef struct
 {
     GPIO_TypeDef *Port;
     uint16_t Pin;
-
 } BSP_GPIO_t;
 
-void BSP_GPIO_Set(BSP_GPIO_t *gpio);
-void BSP_GPIO_Reset(BSP_GPIO_t *gpio);
+typedef enum
+{
+    GPIO_LOW,
+    GPIO_HIGH
+} GPIO_Level_t;
+
+void BSP_GPIO_Write(BSP_GPIO_t *gpio, GPIO_Level_t level);
 void BSP_GPIO_Toggle(BSP_GPIO_t *gpio);
-GPIO_PinState BSP_GPIO_Read(BSP_GPIO_t *gpio);
+GPIO_Level_t BSP_GPIO_Read(BSP_GPIO_t *gpio);
 
 #endif
 
