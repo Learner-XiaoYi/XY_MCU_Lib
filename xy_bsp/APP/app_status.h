14e3fbfb11f3920377f343d605382f055dad42e4
xiaoyi <2965695492@qq.com>
2026-06-22 16:17:18 +0800

    删除调试程序

--- xy_bsp/APP/app_status.h
+++ xy_bsp/APP/app_status.h
@@ -4,14 +4,6 @@
 #include "dev_led.h"
 #include "dev_key.h"
 
-typedef enum
-{
-    APP_STATE_NORMAL,
-    APP_STATE_SETTING,
-    APP_STATE_ALARM
-} app_state_t;
 
-void app_key(void);
-void app_status(void);
 
 #endif
